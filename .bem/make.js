9ced09d0158546f4d0b394e4e4178e4101c51cb4
Kalmakov Alex <alex-k@yandex-team.ru>
2012-11-21 00:23:08 +0400

    +make.js

---
+++ .bem/make.js
@@ -0,0 +1,34 @@
+MAKE.decl('Arch', {
+
+
+    getLibraries: function() {
+
+        return {
+            'bem-bl': {
+                type: 'git',
+                url: 'git://github.com/bem/bem-bl.git',
+                treeish: '0.3'
+            }
+        };
+
+    }
+
+});
+
+
+MAKE.decl('BundleNode', {
+
+    getTechs: function() {
+        return [
+            'bemjson.js',
+            'bemdecl.js',
+            'deps.js',
+            'bemhtml',
+            'js',
+            'css',
+            'ie.css',
+            'html'
+        ];
+    }
+
+});
